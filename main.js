e17b49570bcdc12f5bda2d345770e152368dbbc1
Tobias Zimmermann <info@tzdesign.de>
2016-10-03 16:00:03 +0200

    modes

--- main.js
+++ main.js
@@ -10,12 +10,12 @@ var maxVolume = -205;
 
 
 function percentageToVolume(percentage){
-    return minVolume + ((maxVolume - minVolume) * percentage / 100)
+  return minVolume + ((maxVolume - minVolume) * percentage / 100)
 }
 
 
 function volumeToPercentage(volume){
-    return Math.round((volume - minVolume) / (maxVolume - minVolume) * 100)
+  return Math.round((volume - minVolume) / (maxVolume - minVolume) * 100)
 }
 
 app.get('/volume/:volume',function(req,res){
@@ -28,7 +28,7 @@ app.get('/volume/:volume',function(req,res){
 
 app.get('/on',function(req,res){
   yamaha.powerOn().then(function(){
-      yamaha.setVolume(percentageToVolume(80));
+    yamaha.setVolume(percentageToVolume(80));
   });
   res.send('1');
 });
@@ -49,13 +49,35 @@ app.get('/volume',function(req,res){
     res.send(""+volumeToPercentage(basicInfo.getVolume())+"");
   });
 });
+
 app.get('/is/:mode',function(req,res){
   yamaha.getBasicInfo().done(function(basicInfo){
-    res.send('Volume:' + basicInfo.getVolume());
+    res.send(basicInfo.getCurrentInput()==req.params.mode?"1":"0");
+  });
+});
+
+app.get('/mode',function(req,res){
+  yamaha.getBasicInfo().done(function(basicInfo){
+    res.send(basicInfo.getCurrentInput())
   });
 });
 
 
-  app.listen(port, function () {
-    console.log('Your yamaha is now listening on port '+port+'!');
+app.get('/set/mode/:mode',function(req,res){
+  yamaha.setInputTo(req.params.mode).then(function(){
+    res.send('set to mode '+req.params.mode);
   });
+});
+
+
+app.get('/volume',function(req,res){
+  yamaha.getBasicInfo().done(function(basicInfo){
+    res.send(""+volumeToPercentage(basicInfo.getVolume())+"");
+  });
+});
+
+
+
+app.listen(port, function () {
+  console.log('Your yamaha is now listening on port '+port+'!');
+});
