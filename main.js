02a4b2ddab669d39998ed925583b7edf65409208
Tobias Zimmermann <info@tzdesign.de>
2016-10-03 16:52:18 +0200

    modes

--- main.js
+++ main.js
@@ -27,63 +27,71 @@ app.get('/volume/:volume',function(req,res){
 
 
 app.get('/on',function(req,res){
-  yamaha.isOff().then(function(){
-    yamaha.powerOn().then(function(){
-      yamaha.setVolume(percentageToVolume(80));
-    });
+
+  yamaha.getBasicInfo().done(function(basicInfo){
+    if(basicInfo.isOff()){
+      yamaha.powerOn().then(function(){
+        yamaha.setVolume(percentageToVolume(80));
+      });
+    }
   });
   res.send('1');
 });
 
 
 app.get('/off',function(req,res){
-  yamaha.powerOff();
-  res.send('0');
-});
 
-
-app.get('/status',function(req,res){
-  res.send(yamaha.isOn()?"1":"0");
-});
-
-app.get('/volume',function(req,res){
   yamaha.getBasicInfo().done(function(basicInfo){
-    res.send(""+volumeToPercentage(basicInfo.getVolume())+"");
+    if(basicInfo.isOn()){
+      yamaha.powerOff();
+    }
+    });
+    res.send('0');
   });
-});
 
-app.get('/is/:mode',function(req,res){
-  yamaha.getBasicInfo().done(function(basicInfo){
-    res.send(basicInfo.getCurrentInput()==req.params.mode?"1":"0");
+
+  app.get('/status',function(req,res){
+    res.send(yamaha.isOn()?"1":"0");
   });
-});
 
-app.get('/mode',function(req,res){
-  yamaha.getBasicInfo().done(function(basicInfo){
-    res.send(basicInfo.getCurrentInput())
+  app.get('/volume',function(req,res){
+    yamaha.getBasicInfo().done(function(basicInfo){
+      res.send(""+volumeToPercentage(basicInfo.getVolume())+"");
+    });
   });
-});
 
-app.get('/dummy',function(req,res){
-  res.send('1')
-});
-
-
-app.get('/set/mode/:mode',function(req,res){
-  yamaha.setInputTo(req.params.mode).then(function(){
-    res.send('set to mode '+req.params.mode);
+  app.get('/is/:mode',function(req,res){
+    yamaha.getBasicInfo().done(function(basicInfo){
+      res.send(basicInfo.getCurrentInput()==req.params.mode?"1":"0");
+    });
   });
-});
 
-
-app.get('/volume',function(req,res){
-  yamaha.getBasicInfo().done(function(basicInfo){
-    res.send(""+volumeToPercentage(basicInfo.getVolume())+"");
+  app.get('/mode',function(req,res){
+    yamaha.getBasicInfo().done(function(basicInfo){
+      res.send(basicInfo.getCurrentInput())
+    });
   });
-});
+
+  app.get('/dummy',function(req,res){
+    res.send('1')
+  });
+
+
+  app.get('/set/mode/:mode',function(req,res){
+    yamaha.setInputTo(req.params.mode).then(function(){
+      res.send('set to mode '+req.params.mode);
+    });
+  });
+
+
+  app.get('/volume',function(req,res){
+    yamaha.getBasicInfo().done(function(basicInfo){
+      res.send(""+volumeToPercentage(basicInfo.getVolume())+"");
+    });
+  });
 
 
 
-app.listen(port, function () {
-  console.log('Your yamaha is now listening on port '+port+'!');
-});
+  app.listen(port, function () {
+    console.log('Your yamaha is now listening on port '+port+'!');
+  });
